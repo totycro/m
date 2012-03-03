a2160503cfebf9a4dc3f6b25c95be48af5de1489
totycro <totycro@unknown-horizons.org>
2012-03-03 13:30:04 +0100

    A change of scene, with no regrets

--- mid_to_ogg.sh
+++ mid_to_ogg.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-TIMIDITY_ARGS="--volume=130" 
+TIMIDITY_ARGS="--volume=120" 
+#TIMIDITY_ARGS=""
 if test $# -gt 0; then
 	echo $@
 	find . -iname "*$@*.mid" | cut -d'/' -f 2 | while read i ; do
