30725e721637ac913f48dccae92c1b6d9d5b8687
totycro <totycro@unknown-horizons.org>
2012-03-12 21:59:38 +0100

    base

--- mid_to_ogg.sh
+++ mid_to_ogg.sh
@@ -1,21 +1,32 @@
 #!/bin/sh
+
 TIMIDITY_ARGS="--volume=120" 
 #TIMIDITY_ARGS=""
-if test $# -gt 0; then
-	echo $@
-	find . -iname "*$@*.mid" | cut -d'/' -f 2 | while read i ; do
+j=0
+/home/totycro/bin/dropbox.py stop
+find . -iname "*$@*.mid" | cut -d'/' -f 2 | while read i ; do
 	echo $i
+	if [ $((j%3)) -eq 2 ] ; then
+		echo 1
+ 		# run one now really
 		timidity $TIMIDITY_ARGS -Aa -Ov "$i" 
-	done
-else
-	ls *.mid | while read i ; do
-		timidity $TIMIDITY_ARGS -Aa -Ov "$i" 
-	done
-fi
+	else
+		echo 2
+		timidity $TIMIDITY_ARGS -Aa -Ov "$i"  &
+		LAST_PID=$!
+	fi
+	let j+=1
+done
+
+wait $LAST_PID
+
+
+/home/totycro/bin/dropbox.py start
 
 # STUPID HACK:
 # name files back, default output converts . to _
+echo "rename.."
 find . -iname "*.ogg" | while read i ; do
-	mv "$i" "`echo $i | tr _ .`"
+	mv "$i" "`echo $i | tr _ .`" 2>/dev/null
 done
 
