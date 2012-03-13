a0928a785a0c757d4e6b2226d21a83e4b02f6044
totycro <totycro@unknown-horizons.org>
2012-03-13 01:51:17 +0100

    gestalt

--- mid_to_ogg.sh
+++ mid_to_ogg.sh
@@ -4,23 +4,37 @@ TIMIDITY_ARGS="--volume=120"
 #TIMIDITY_ARGS=""
 j=0
 /home/totycro/bin/dropbox.py stop
+
+D="/tmp/foooobarr"
+mkdir "$D"
+OUT="${D}/fiiifooo_not_rlly"
+# can't pass data out of find | while read subshell, so stupidly use files
+DAT="${D}/dat"
+rm -f $OUT
+rm -f $DAT
+
+touch $DAT
+touch $OUT
+
+# wait for every 3rd job, so that not all too many run at the same time
 find . -iname "*$@*.mid" | cut -d'/' -f 2 | while read i ; do
 	echo $i
 	if [ $((j%3)) -eq 2 ] ; then
-		#echo 1
  		# run one now really
-		timidity $TIMIDITY_ARGS -Aa -Ov "$i" 
+		nice timidity $TIMIDITY_ARGS -Aa -Ov "$i" 
 	else
-		#echo 2
-		# TODO waiting doesn't work yet
-		timidity $TIMIDITY_ARGS -Aa -Ov "$i"  # &
-		LAST_PID=$!
+		# reg job, call out when finished
+		echo "job" >> "$DAT"
+		(nice timidity $TIMIDITY_ARGS -Aa -Ov "$i"; echo "done" >> "$OUT" )  &
 	fi
 	let j+=1
 done
 
-#wait $LAST_PID
 
+while [ "x$(cat "$DAT" | wc -l)" != "x$(cat "$OUT" | wc -l)" ]; do
+	echo "waiting for tasks.."
+	sleep 5
+done
 
 /home/totycro/bin/dropbox.py start
 
