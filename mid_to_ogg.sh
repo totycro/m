03bb9cea0afdff96669cd9ade2d865ade62ac975
Bernhard Mallinger <b.mallinger@gmx.at>
2017-12-09 23:24:46 +0100

    added proper ogg tags

--- mid_to_ogg.sh
+++ mid_to_ogg.sh
@@ -16,32 +16,40 @@ rm -f $DAT
 touch $DAT
 touch $OUT
 
-# wait for every 3rd job, so that not all too many run at the same time
-find . -iname "*$@*.mid" | cut -d'/' -f 2 | while read i ; do
-	echo $i
-	if [ $((j%3)) -eq 2 ] ; then
- 		# run one now really
-		nice timidity $TIMIDITY_ARGS -Aa -Ov "$i" 
-	else
-		# reg job, call out when finished
-		echo "job" >> "$DAT"
-		(nice timidity $TIMIDITY_ARGS -Aa -Ov "$i"; echo "done" >> "$OUT" )  &
-	fi
-	let j+=1
-done
-
-
-while [ "x$(cat "$DAT" | wc -l)" != "x$(cat "$OUT" | wc -l)" ]; do
-	echo "waiting for tasks.."
-	sleep 2
-done
+## wait for every 3rd job, so that not all too many run at the same time
+#find . -iname "*$@*.mid" | cut -d'/' -f 2 | while read i ; do
+#	echo $i
+#	if [ $((j%3)) -eq 2 ] ; then
+# 		# run one now really
+#		nice timidity $TIMIDITY_ARGS -Aa -Ov "$i" 
+#	else
+#		# reg job, call out when finished
+#		echo "job" >> "$DAT"
+#		(nice timidity $TIMIDITY_ARGS -Aa -Ov "$i"; echo "done" >> "$OUT" )  &
+#	fi
+#	let j+=1
+#done
+#
+#
+#while [ "x$(cat "$DAT" | wc -l)" != "x$(cat "$OUT" | wc -l)" ]; do
+#	echo "waiting for tasks.."
+#	sleep 2
+#done
 
 #dropbox start
 
 # STUPID HACK:
 # name files back, default output converts . to _
+# and tag
 echo "rename.."
 find . -iname "*.ogg" | while read i ; do
-	mv "$i" "`echo $i | tr _ .`" 2>/dev/null 
+
+  new_filename="`echo $i | tr _ .`"
+	mv "$i" $new_filename  2>/dev/null 
+
+  vorbiscomment -w "$new_filename" -t "ARTIST=totycro" -t "TITLE=$(echo ${i%.ogg} | tr /_. '   ')"
+
 done
+
+
 
